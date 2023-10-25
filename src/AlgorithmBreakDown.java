3bc1164a980d02dc920beb8685cb00e3a472a3f7
Ritesh Shah <arits.king74@gmail.com>
2023-10-25 13:32:10 -0500

    changes in functionality

--- src/AlgorithmBreakDown.java
+++ src/AlgorithmBreakDown.java
@@ -1,5 +1,7 @@
-public class AlgorithmBreakDown {
+import java.util.Currency;
+import java.util.Scanner;
 
+public class AlgorithmBreakDown {
     private double currentPopularityPercentageOfThePLayer;
     private double percentageChangeAfterGettingPopularity;
     private double totalPopularity;
@@ -7,13 +9,26 @@ public class AlgorithmBreakDown {
 
 
     public double calculateTotal(double currentPopularityPercentageOfThePLayer, double percentageChangeAfterGettingPopularity, double popularityAboutToBeDonated){
-totalPopularity=(100*popularityAboutToBeDonated-((currentPopularityPercentageOfThePLayer+100*percentageChangeAfterGettingPopularity)*popularityAboutToBeDonated))/(currentPopularityPercentageOfThePLayer*100);
+totalPopularity=(popularityAboutToBeDonated*(100 -currentPopularityPercentageOfThePLayer-percentageChangeAfterGettingPopularity))/percentageChangeAfterGettingPopularity;
         return totalPopularity;
     }
+public  double whatPercentageAfterDonation(double popularityPercentageOfThePLayerBeforeDonation, double totalPopularity, double popularityAboutToBeDonated){
 
+        return (popularityAboutToBeDonated*(100-popularityPercentageOfThePLayerBeforeDonation))/(totalPopularity+popularityAboutToBeDonated)+ currentPopularityPercentageOfThePLayer;
+}
     public static void main(String[] args) {
         AlgorithmBreakDown ab= new AlgorithmBreakDown();
-        System.out.println("Total popularity after donation: "+(ab.calculateTotal(26,1,100)+ ab.popularityAboutToBeDonated));
+        Scanner sc= new Scanner(System.in);
+        System.out.println("How much popularity did you donate to find the required details?");
+        double popularityDonatedToFindDetails= sc.nextDouble();
+        System.out.println("What's the current popularity percentage?");
+        double currentPercentage= sc.nextDouble();
+        System.out.println("What was the percentage change after you donated the popularity?");
+        double percentageChange= sc.nextDouble();
+        double percentageBeforeDonation= currentPercentage-percentageChange;
+
+
+        System.out.println("Total popularity after donation: "+((ab.calculateTotal(percentageBeforeDonation,percentageChange,popularityDonatedToFindDetails))+ popularityDonatedToFindDetails));
     }
 
 }
